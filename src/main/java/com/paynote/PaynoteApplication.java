c7097041c22727ac7fc738a273838c6a8e77cdf1
남동석 <ehdtka456@naver.com>
2025-12-21 21:30:31 +0900

    first commit

---
+++ src/main/java/com/paynote/PaynoteApplication.java
@@ -0,0 +1,13 @@
+package com.paynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaynoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaynoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paynote/PaynoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaynoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
